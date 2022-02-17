d0160bccc33000b2e1be92c7cbc5fd58eb6d6f77
SurSouthside <Stuart.SouthIII1@gmail.com>
2022-02-17 10:40:26 -0500

    Generate ability scores using 3d6 method

--- charcreate-client/src/components/AbilityScores.vue
+++ charcreate-client/src/components/AbilityScores.vue
@@ -47,16 +47,51 @@ export default {
   methods:{
     generateAbilityScores() {
         //alert(this.abilityGenType);
+        this.strScore = 0;
+        this.dexScore = 0;
+        this.conScore = 0;
+        this.intScore = 0;
+        this.wisScore = 0;
+        this.chaScore = 0;
         switch(this.abilityGenType) {
          case '3d6':
           //alert("3d6");
           for(let i = 0; i < 3; i++) {
            var strRoll = Math.floor(Math.random() * (this.diceMax - this.diceMin) + this.diceMin);
-           alert("Dice roll: " + strRoll);
+           //alert("Dice roll: " + strRoll);
            this.strScore = this.strScore + strRoll;
-           alert("Strength score: " + this.strScore);
+           //alert("Strength score: " + this.strScore);
           }
-          //alert(this.strScore);
+          for(let i = 0; i < 3; i++) {
+           var dexRoll = Math.floor(Math.random() * (this.diceMax - this.diceMin) + this.diceMin);
+           //alert("Dice roll: " + dexRoll);
+           this.dexScore = this.dexScore + dexRoll;
+           //alert("Dexterity score: " + this.dexScore);
+          }
+          for(let i = 0; i < 3; i++) {
+           var conRoll = Math.floor(Math.random() * (this.diceMax - this.diceMin) + this.diceMin);
+           //alert("Dice roll: " + conRoll);
+           this.conScore = this.conScore + conRoll;
+           //alert("Constitution score: " + this.conScore);
+          }
+          for(let i = 0; i < 3; i++) {
+           var intRoll = Math.floor(Math.random() * (this.diceMax - this.diceMin) + this.diceMin);
+           //alert("Dice roll: " + intRoll);
+           this.intScore = this.intScore + intRoll;
+           //alert("Intelligence score: " + this.intScore);
+          }
+          for(let i = 0; i < 3; i++) {
+           var wisRoll = Math.floor(Math.random() * (this.diceMax - this.diceMin) + this.diceMin);
+           //alert("Dice roll: " + wisRoll);
+           this.wisScore = this.wisScore + wisRoll;
+           //alert("Wisdom score: " + this.wisScore);
+          }   
+          for(let i = 0; i < 3; i++) {
+           var chaRoll = Math.floor(Math.random() * (this.diceMax - this.diceMin) + this.diceMin);
+           //alert("Dice roll: " + chaRoll);
+           this.chaScore = this.chaScore + chaRoll;
+           //alert("Charisma score: " + this.chaScore);
+          }                        
           break;
          case '4d6':
           alert("4d6");
